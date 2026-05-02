2b276c62daaa1caa5bbdb49366ffbcbcb0b0df94
Ronan Fernandes <ronanlm10@hotmail.com>
2026-05-02 11:30:29 -0300

    feat: Set up packaging resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,13 @@ android {
             )
         }
     }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
